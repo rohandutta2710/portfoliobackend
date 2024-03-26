dd039ca7c95c700b003fbe3b90b7611025b07932
Rohan Dutta <rohandutta27102000@gmail.com>
2024-03-26 15:33:40 +0530

    2.2

--- backend.js
+++ backend.js
@@ -46,10 +46,10 @@ app.post("/contact", (req, res) => {
     let data = { ...req.query, date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`, time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` };
     var mailOptions = {
         from: process.env.Email_ID,
-        to: data.email+",rohandutta27102000@gmail.com",
+        to: data.email + ",rohandutta27102000@gmail.com",
         subject: 'Get in Touch with Rohan!',
-        html: "<p>Hi "+data.name+", <br/><br/>Our support team will connect you soon. Request you to check your mailbox regularly for further updates."+
-        "<br/><br/><b>Request ID:-</b>"+uuid.v4() +"<br/><b>Your message:-<br/></b> "+data.message +"<br/><br/>Thanks,<br/>RDX Team</p>"
+        html: "<p>Hi " + data.name + ", <br/><br/>Our support team will connect you soon. Request you to check your mailbox regularly for further updates." +
+            "<br/><br/><b>Request ID:-</b>" + uuid.v4() + "<br/><b>Your message:-<br/></b> " + data.message + "<br/><br/>Thanks,<br/>RDX Team</p>"
     };
     // console.log(data);
     transporter.sendMail(mailOptions, function (error, info) {
@@ -61,6 +61,6 @@ app.post("/contact", (req, res) => {
     });
     // res.send("<h1> Response is saved. Rohan Dutta will reach you asap.<br><a href='http://localhost:3000/'>Back to page</a></h1>");
 })
-app.listen(ports, "localhost", () => {
+app.listen(ports, () => {
     console.log("Server is running.")
 })
